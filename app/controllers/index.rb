fd587b92b3a41a80d1f33dca6fc4cb74e6c4bbd1
Tim Cannady <tinmcannady@gmail.com>
2015-07-11 21:10:16 -0700

    change some directories, add framework for users view and routing

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,22 +2,5 @@ get '/' do
   erb :index
 end
 
-post '/users' do
-  erb :index
-end
 
-post '/users/create' do
-  create_user
-  login
-  redirect '/'
-end
 
-post '/users/login' do
-  login
-  redirect '/'
-end
-
-post '/users/logout' do
-  logout
-  redirect '/'
-end
---
+++ app/controllers/users.rb
@@ -0,0 +1,29 @@
+get '/users/:id' do
+  # @user = User.find(params[:id])
+  erb :'user/user'
+end
+
+get '/users' do
+  # @all_users = User.all
+  erb :'user/users_all'
+end
+
+post '/users' do
+  erb :index
+end
+
+post '/users/create' do
+  create_user
+  login
+  redirect '/'
+end
+
+post '/users/login' do
+  login
+  redirect '/'
+end
+
+post '/users/logout' do
+  logout
+  redirect '/'
+end
