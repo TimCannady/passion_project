05ee4032e50708955b241ec787fe78cd3d6f30ec
Tim Cannady <timcannady@gmail.com>
2015-07-12 18:32:43 -0700

    refactor index route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,9 @@
 get '/' do
 
   if logged_in
-    puts "logged in!"
-    establish_current_user
-    redirect "/users/#{@current_user.id}"
+    redirect "/users/home"
+    # establish_current_user
+    # redirect "/users/#{@current_user.id}"
   else
     erb :index
   end
